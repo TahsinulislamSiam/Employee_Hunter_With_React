b0b27b38bee6f892aedc1b95158df6fcd4585f23
Tahsinul islam <mohammadsiam8760@gmail.com>
2026-01-03 19:59:41 +0600

    fixing some bugs part 1

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios'
+import { useAuth } from '../context/authContext';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail]= useState('')
     const [password, setPassword]=useState('')
     const [error, setError] = useState(null)
+    const {Login} = useAuth()
+    const navigate = useNavigate()
 
     const handleSubmit = async(e)=>{
         e.preventDefault()
@@ -12,7 +16,13 @@ const Login = () => {
             const response = await axios.post("http://localhost:5000/api/auth/login",
                 {email,password});
             if(response.data.success){
-                alert("Successfully Login")
+               Login(response.data.user)
+                localStorage.setItem("token",response.data.token)
+               if(response.data.user.role=="admin") {
+                navigate('/admin-dashboard')
+               } else{
+                navigate('/employee-dashboard')
+               }
             }
         }
         catch(error){
